f192eea86b112870b280c7f9daec0e2ca7420bf6
sliman4 <4sliman4@gmail.com>
2022-06-06 20:34:53 +0300

    Удалил REACT_APP_INFURA_PROJECT_ID

--- src/helpers/utilities.ts
+++ src/helpers/utilities.ts
@@ -111,22 +111,13 @@ export function getChainData(chainId: number): IChainData {
     throw new Error("ChainId missing or not supported");
   }
 
-  const API_KEY = process.env.REACT_APP_INFURA_PROJECT_ID;
-
-  if (!API_KEY) {
-    throw new Error("Environment variable REACT_APP_INFURA_PROJECT_ID is not set");
-  }
-
   if (
     chainData.rpc_url.includes("infura.io") &&
-    chainData.rpc_url.includes("%API_KEY%") &&
-    API_KEY
+    chainData.rpc_url.includes("%API_KEY%")
   ) {
-    const rpcUrl = chainData.rpc_url.replace("%API_KEY%", API_KEY);
-
     return {
       ...chainData,
-      rpc_url: rpcUrl,
+      rpc_url: chainData.rpc_url,
     };
   }
 
