58569ad2e1c32735453df15fb451a6ea625c4c1c
Sliman4 <Sliman4@users.noreply.github.com>
2022-06-06 17:32:20 +0000

    deploy: 2ec2c746bd42a690f451c12abf60d65737f8943f

---
+++ precache-manifest.cbcff05c2572ee82b48ef57a3409b7a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d140d66e61ef97a26ac",
+    "url": "/walletconnect-test-wallet/static/js/main.3d140d66.chunk.js"
+  },
+  {
+    "revision": "76584563435e127d0f05",
+    "url": "/walletconnect-test-wallet/static/js/1.76584563.chunk.js"
+  },
+  {
+    "revision": "f01159b1d574c5b4cd77",
+    "url": "/walletconnect-test-wallet/static/js/runtime~main.f01159b1.js"
+  },
+  {
+    "revision": "e1cb8d21bffbcc2a8c7bfcf95bad12ff",
+    "url": "/walletconnect-test-wallet/static/media/walletconnect-logo.e1cb8d21.png"
+  },
+  {
+    "revision": "14add5afa08e961906485690d90cb237",
+    "url": "/walletconnect-test-wallet/index.html"
+  }
+];
